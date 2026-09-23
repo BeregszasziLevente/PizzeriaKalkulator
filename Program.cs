959e8be098019661cbef07082e3808a3b2953e28
BeregszasziLevente <beregszaszi.levente.2024d@telekimezotur.hu>
2026-09-23 12:27:52 +0200

    3. feladat

--- Program.cs
+++ Program.cs
@@ -3,3 +3,10 @@ string nev = "Tóth Bence";
 int osszeg = 8500;
 bool torzs = false;
 
+int szallDij;
+
+if (osszeg >= 10000 || torzs==true) { szallDij = 0; }
+else if (osszeg >=5000) { szallDij = 500; }
+else { szallDij = 1200; }
+
+int vegossz = osszeg + szallDij;
