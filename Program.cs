f87ec9345ce336e63143e9fdd30966f875590ac7
BeregszasziLevente <beregszaszi.levente.2024d@telekimezotur.hu>
2026-09-23 12:32:36 +0200

    4. feladat

--- Program.cs
+++ Program.cs
@@ -10,3 +10,11 @@ else if (osszeg >=5000) { szallDij = 500; }
 else { szallDij = 1200; }
 
 int vegossz = osszeg + szallDij;
+
+Console.Write("======================================================");
+Console.WriteLine();
+Console.WriteLine($"Vásárló neve: {nev}");
+Console.WriteLine($"Rendelés összege: {osszeg} Ft");
+Console.WriteLine($"Szállítási díj: {szallDij} Ft (Törzsvásárlói / 10k feletti ingyenes szállítás)");
+Console.WriteLine($"Fizetendő végösszeg: {vegossz} Ft");
+Console.Write("======================================================");
